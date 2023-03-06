7b1508ac71bd0ad0cd641157945d04535f5bcddc
Enzo Neault <aza.woodyy@gmail.com>
2023-03-06 10:26:44 +0100

    fix + add new functionnality

--- src/Service/CharacterServiceInterface.php
+++ src/Service/CharacterServiceInterface.php
@@ -4,7 +4,13 @@ use App\Entity\Character;
 interface CharacterServiceInterface
 {
     # Creates the character
-    public function create();
+    public function create(string $data);
+
+    public function isEntityFilled(Character $character);
+
+    public function submit(Character $character, $formName, $data);
+
+    public function modify(Character $character, string $data);
 
     # Retrieve a character
     public function findOneByIdentifier(string $identifier): Character;
